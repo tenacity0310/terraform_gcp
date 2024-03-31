0e39dcea8e23a8590a30103d831e8ff29a65cf9a
Peter Li <thpeterli@gmail.com>
2024-03-31 16:02:24 +0800

    add

---
+++ ch4.7.4-lab8/0-provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  # specify version  of terraform 
+  required_version = ">=1.7.0"
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      # specify version of google provider version
+      version = ">=5.15.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.GCP_PROJECT
+}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+  token                  = data.google_client_config.default.access_token
+}
